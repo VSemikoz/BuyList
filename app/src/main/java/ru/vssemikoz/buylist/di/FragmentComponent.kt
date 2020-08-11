ba43f807fa4f412bcf135ae2e0dbc0da60584e41
VSemikoz <slava.semik@yandex.ru>
2020-08-12 01:36:45 +0300

    add di for AddEdit fragment and presenter

--- app/src/main/java/ru/vssemikoz/buylist/di/FragmentComponent.kt
+++ app/src/main/java/ru/vssemikoz/buylist/di/FragmentComponent.kt
@@ -1,6 +1,7 @@
 package ru.vssemikoz.buylist.di
 
 import dagger.Subcomponent
+import ru.vssemikoz.buylist.addEditProduct.AddEditProductFragment
 import ru.vssemikoz.buylist.productLists.ProductListsFragment
 
 
@@ -8,11 +9,13 @@ import ru.vssemikoz.buylist.productLists.ProductListsFragment
     AdapterModule::class,
     PresenterModule::class,
 //    RepositoryModule.class,
-    StorageModule::class
+    StorageModule::class,
 //    IconicStorageModule.class,
-//    NavigatorModule.class,
+    NavigatorModule::class
 //    MapperModule.class
 ])
 public interface FragmentComponent {
     fun inject(fragment: ProductListsFragment?)
+
+    fun inject(fragment: AddEditProductFragment?)
 }
--- app/src/main/java/ru/vssemikoz/buylist/di/PresenterModule.kt
+++ app/src/main/java/ru/vssemikoz/buylist/di/PresenterModule.kt
@@ -2,13 +2,20 @@ package ru.vssemikoz.buylist.di
 
 import dagger.Module
 import dagger.Provides
+import ru.vssemikoz.buylist.addEditProduct.AddEditProductContract
+import ru.vssemikoz.buylist.addEditProduct.AddEditProductPresenter
 import ru.vssemikoz.buylist.productLists.ProductListsContract
 import ru.vssemikoz.buylist.productLists.ProductListsPresenter
 
 @Module
 class PresenterModule {
     @Provides
-    fun provideNewsFeedPresenter(presenter: ProductListsPresenter): ProductListsContract.Presenter {
+    fun provideProductListPresenter(presenter: ProductListsPresenter): ProductListsContract.Presenter {
+        return presenter
+    }
+
+    @Provides
+    fun provideAddEditPresenter(presenter: AddEditProductPresenter): AddEditProductContract.Presenter {
         return presenter
     }
 }
