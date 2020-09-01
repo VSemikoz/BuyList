5b1418ecc81d209792c78f9033c9929187d3dd1a
VSemikoz <slava.semik@yandex.ru>
2020-09-01 20:14:17 +0300

    add Category : Serializable

--- app/src/main/java/ru/vssemikoz/buylist/models/Category.kt
+++ app/src/main/java/ru/vssemikoz/buylist/models/Category.kt
@@ -2,10 +2,11 @@ package ru.vssemikoz.buylist.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity
 data class Category(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
     var name: String
-)
+) : Serializable
