d36d9895c2b461977db4d26e62270afd9e418476
VSemikoz <slava.semik@yandex.ru>
2020-09-01 20:15:47 +0300

    correct Navigator impl add openAddEditCategory()

--- app/src/main/java/ru/vssemikoz/buylist/utils/navigator/Navigator.kt
+++ app/src/main/java/ru/vssemikoz/buylist/utils/navigator/Navigator.kt
@@ -1,11 +1,20 @@
 package ru.vssemikoz.buylist.utils.navigator
 
+import ru.vssemikoz.buylist.addEditCategoryDialog.AddEditCategoryDialogFragment
+import ru.vssemikoz.buylist.categoryList.CategoryListFragment
+import ru.vssemikoz.buylist.models.Category
+import ru.vssemikoz.buylist.models.Product
+
 
 interface Navigator {
 
-    fun openAddEditProduct(productId: Int?)
-
-    fun openAddEditCategory(categoryId: Int?)
+    fun openAddEditProduct(product: Product?)
 
     fun openCategoryList()
+
+    fun openAddEditCategory(
+        category: Category?,
+        fragment: CategoryListFragment,
+        listener: AddEditCategoryDialogFragment.OnButtonClickedListener
+    )
 }
--- app/src/main/java/ru/vssemikoz/buylist/utils/navigator/NavigatorImpl.kt
+++ app/src/main/java/ru/vssemikoz/buylist/utils/navigator/NavigatorImpl.kt
@@ -2,31 +2,41 @@ package ru.vssemikoz.buylist.utils.navigator
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import ru.vssemikoz.buylist.addEditCategoryDialog.AddEditCategoryDialogFragment
 import ru.vssemikoz.buylist.addEditProduct.AddEditProductActivity
 import ru.vssemikoz.buylist.categoryList.CategoryListActivity
+import ru.vssemikoz.buylist.categoryList.CategoryListFragment
+import ru.vssemikoz.buylist.models.Category
+import ru.vssemikoz.buylist.models.Product
 import javax.inject.Inject
 
 class NavigatorImpl @Inject constructor(val context: Context) : Navigator {
-
-    override fun openAddEditProduct(productId: Int?) {
+    override fun openAddEditProduct(product: Product?) {
         Intent(context, AddEditProductActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            if (productId != null) {
-                putExtra("productId", productId)
+            if (product != null) {
+                val bundle = Bundle()
+                bundle.putSerializable("Product", product)
+                putExtras(bundle)
             }
             context.startActivity(this)
         }
     }
 
-    override fun openAddEditCategory(categoryId: Int?) {
-        TODO()
-//        Intent(context, AddEditCategoryActivity::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-//            if (categoryId != null) {
-//                putExtra("productId", categoryId)
-//            }
-//            context.startActivity(this)
-//        }
+    override fun openAddEditCategory(
+        category: Category?,
+        fragment: CategoryListFragment,
+        listener: AddEditCategoryDialogFragment.OnButtonClickedListener
+    ) {
+        AddEditCategoryDialogFragment(listener).apply {
+            if (category != null){
+                val bundle = Bundle()
+                bundle.putSerializable("Category", category)
+                arguments = bundle
+            }
+            show(fragment.fragmentManager, "addEditCategoryDialog")
+        }
     }
 
     override fun openCategoryList() {
